5ed38f46e855517f7962179f4bea07a78a6cf496
Bill McCarvell <bill@syntaxis.net>
2013-04-24 13:25:56 -0400

    added group and some calculation attributes

--- WAK-AddressBook/Model.js
+++ WAK-AddressBook/Model.js
@@ -1,4 +1,28 @@
 ﻿
 guidedModel =// @startlock
 {
+	Person :
+	{
+		entityMethods :
+		{// @endlock
+			fullLastFirst:function()
+			{// @lock
+				// Add your code here
+			},// @lock
+			fullFirstLast:function()
+			{// @lock
+				// Create a fullFirstLast from 
+				return firstName + " " + lastName
+			},// @lock
+			displayName:function()
+			{// @lock
+				if(ds.Person.isCompany == True){
+					return fullName
+				} else {
+					return companyName
+				}
+			
+			}// @startlock
+		}
+	}
 };// @endlock
