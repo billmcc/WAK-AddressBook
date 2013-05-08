de775e77842b97de3ac36da30601c0db2e22aae9
Bill McCarvell <bill@syntaxis.net>
2013-05-08 14:36:04 -0400

    added admin class

---
+++ WAK-AddressBook/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,34 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Find out how to display the list (lastname, first etc) from settings
+		 
+		// create an array with 'All Contacts' at the top
+		
+		
+		var aGroupList = new Array();
+		mycars[0] = "All contacts...";
+		
+
+		// Query all persons for this admin
+		
+		// Load all persons into the array
+		
+		
+		
+		
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
