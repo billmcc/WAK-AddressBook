22eb8b65c874992b8d8929b61a586e4a53b49ac9
Bill McCarvell <bill@syntaxis.net>
2013-06-11 15:35:54 -0400

    minor update

--- WAK-AddressBook/WebFolder/index.waPage/scripts/index.js
+++ WAK-AddressBook/WebFolder/index.waPage/scripts/index.js
@@ -20,7 +20,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-		
+		debugger;
 		if(WAF.directory.currentUser() != null){
 			//take action for a logged in request
 			$$("main").loadComponent("/components/app.waComponent");
@@ -28,11 +28,13 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 			//take action for a non logged in request
 			$$("main").loadComponent("/components/public.waComponent");
 		}
-
+		
 		//Load the current Admin
 		//Who is the currentAdmin? - obtained from the login - for now 
-		//Load the ID for the current user		
-		var currentAdminID = 1; //which is me
+		//Load the ID for the current user	
+		//Get the currentUser and load the UUID for that user
+		//Query	the Admin class - directoryID = 
+		var currentAdminID = WAF.directory.currentUser().ID; //which is me
 		
 		//load var with list type (how the contact list displays the information, i.e. first name last or last name first)
 		//find the current class Admin and load the value
@@ -40,11 +42,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		//load the grid list of groups
 		var GroupsList = new Array();
 		GroupsList[0] = "All contacts...";
-		$$('dataGrid2').setValue(GroupsList);
+		$$(getHtmlId('GroupListGrid')).setValue(GroupsList);
 		//load the contact list with all the contacts for the user
-
-		debugger;
- 
 
 
 		// Query all persons for this admin
